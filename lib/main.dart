313af79356616b50ae9b17266c182ec121cb2fed
ma4louf <cgm19@mail.aub.edu>
2023-03-30 16:26:58 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const BookMe());
-
 }
 
 class BookMe extends StatelessWidget {
